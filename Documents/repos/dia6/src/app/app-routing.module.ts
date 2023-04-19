b2974b5a57cdcdf5175fe31a72e8e5465e27d33f
pabloGCtek <pablo.garciacervigon@softtek.com>
2023-04-19 15:30:24 +0200

    commit 1

--- Documents/repos/dia6/src/app/app-routing.module.ts
+++ Documents/repos/dia6/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { ContactarComponent } from './contactar/contactar.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { QuienesComponent } from './quienes/quienes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: InicioComponent},
+  {path: 'contactar', component: ContactarComponent},
+  {path: 'crud', component: FormularioComponent},
+  {path: 'quienes-somos', component: QuienesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Documents/repos/dia6/src/app/app.module.ts
+++ Documents/repos/dia6/src/app/app.module.ts
@@ -4,17 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { FormularioComponent } from './formulario/formulario.component';
+import { CRUDService } from './crud.service';
+import { InicioComponent } from './inicio/inicio.component';
+import { QuienesComponent } from './quienes/quienes.component';
+import { ContactarComponent } from './contactar/contactar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent,
+    InicioComponent,
+    QuienesComponent,
+    ContactarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [CRUDService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
